ead39f618404469d2c60489181e25b37e7dcf084
Sherwin Bularon <20190016985@my.xu.edu.ph>
2022-07-28 23:51:58 +0800

    a

---
+++ About/About/app.js
@@ -0,0 +1,23 @@
+const navSlide = () => {
+    const burger = document.querySelector('.toggle-btn');
+    const nav = document.querySelector('.ul-nav-links');
+    const navLinks = document.querySelectorAll('.ul-nav-links li');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        navLinks.forEach ((link, index) => {
+            if(link.style.animation){
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+
+        burger.classList.toggle('toggle');
+    });
+
+
+}
+
+
+navSlide()
---
+++ app.js
@@ -0,0 +1,79 @@
+const navSlide = () => {
+    const burger = document.querySelector('.toggle-btn');
+    const nav = document.querySelector('.ul-nav-links');
+    const navLinks = document.querySelectorAll('.ul-nav-links li');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        navLinks.forEach ((link, index) => {
+            if(link.style.animation){
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+
+        burger.classList.toggle('toggle');
+    });
+
+
+}
+
+navSlide()
+/*  Clock JS */
+
+window.addEventListener("load", () => {
+    clock();
+    function clock() {
+      const today = new Date();
+  
+      // get time components
+      const hours = today.getHours();
+      const minutes = today.getMinutes();
+  
+      //add '0' to hour, minute & second when they are less 10
+      const hour = hours < 10 ? "0" + hours : hours;
+      const minute = minutes < 10 ? "0" + minutes : minutes;
+  
+      //make clock a 12-hour time clock
+      const hourTime = hour > 12 ? hour - 12 : hour;
+  
+      // if (hour === 0) {
+      //   hour = 12;
+      // }
+      //assigning 'am' or 'pm' to indicate time of the day
+      const ampm = hour < 12 ? "AM" : "PM";
+  
+      // get date components
+      const month = today.getMonth();
+      const year = today.getFullYear();
+      const day = today.getDate();
+  
+      //declaring a list of all months in  a year
+      const monthList = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ];
+  
+      //get current date and time
+      const date = monthList[month] + " " + day + ", " + year;
+      const time = hourTime + ":" + minute + ":" + ampm;
+  
+      //combine current date and time
+      const dateTime = date + " | " + time;
+  
+      //print current date and time to the DOM
+      document.getElementById("date-time").innerHTML = dateTime;
+      setTimeout(clock, 1000);
+    }
+  });
